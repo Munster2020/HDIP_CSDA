98cbf49428c151f0772900a6d295cc784293395c
Munster2020 <g00387820@gmit.ie>
2020-02-05 13:12:32 +0000

    Added two more programs to labs

---
+++ labs/Topic03-variables/lab03.04-random.py
@@ -0,0 +1,7 @@
+# Program that prints a random number between 1 and 10
+# outputs the integer answer and remainder
+
+import random
+
+number = random.randint (1,10)
+print ("Here is a random number {}".format (number))
