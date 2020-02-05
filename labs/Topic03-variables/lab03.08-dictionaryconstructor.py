967246c8bb93305b11012447187b766251b9525e
Munster2020 <g00387820@gmit.ie>
2020-02-05 14:41:11 +0000

    Added dictionary programs

---
+++ labs/Topic03-variables/lab03.08-dictionaryconstructor.py
@@ -0,0 +1,7 @@
+#student = dict (firstname = "joe", lastname = "bloggs")
+#print (student)
+
+student = dict(firstname ="joe", lastname ="bloggs")
+# note that keywords are not string literals
+# note the use of equals rather than colon for the assignment
+print(student)
